03aa1ffa61eb5711a58c2b320f1de1ac6e5cd088
ameya-6964 <belvalkar7@gmail.com>
2024-08-18 02:21:19 +0530

    Understood Working Of Modules Through IIFE

---
+++ episode-5-node-js-working/app.js
@@ -0,0 +1,29 @@
+// When this line runs, Node.js executes the wrapped IIFE from calculate.js
+const calculate = require('./calculate.js');
+
+/*                      Working Of IIFE Behind The Scenes
+
+(function (exports, require, module, __filename, __dirname) {
+    // The original content of calculate.js goes here
+
+    function add(a, b) {
+        return a + b;
+    }
+    function subtract(a, b) {
+        return a - b;
+    }
+
+    module.exports = {
+        add,       // shorthand for 'add: add'
+        subtract   // shorthand for 'subtract: subtract'
+    };
+
+})(module.exports, require, module, __filename, __dirname);
+
+*/
+
+const sum = calculate.add(5, 3);        // sum is 8
+const difference = calculate.subtract(10, 4);  // difference is 6
+
+console.log('Sum:', sum);               // Output: Sum: 8
+console.log('Difference:', difference); // Output: Difference: 6
